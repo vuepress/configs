20f06c5aeefd7a6975640586d048f217d7fc7537
meteorlxy <meteor.lxy@foxmail.com>
2019-11-28 15:46:50 +0800

    feat(eslint-config-vuepress-typescript): extends from base config

--- packages/eslint-config-vuepress-typescript/index.js
+++ packages/eslint-config-vuepress-typescript/index.js
@@ -1,41 +1,21 @@
 module.exports = {
   extends: [
-    'standard',
+    'vuepress',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
-    'plugin:vue/recommended',
-    'prettier',
-    'prettier/standard',
     'prettier/@typescript-eslint',
-    'prettier/vue',
   ],
 
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
 
-  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
 
   rules: {
-    'vue/component-name-in-template-casing': [
-      'error',
-      'PascalCase',
-      {
-        registeredComponentsOnly: false,
-        ignores: [],
-      },
-    ],
-    'vue/match-component-file-name': [
-      'error',
-      {
-        extensions: ['js', 'jsx', 'ts', 'tsx', 'vue'],
-        shouldMatchCase: false,
-      },
-    ],
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': 'error',
     'import/prefer-default-export': 'off',
     'import/no-cycle': 'off',
-    'prettier/prettier': 'error',
   },
 }
