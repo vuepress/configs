ce60c2dcb2f87b0b6d61def0f119caf74640bce9
meteorlxy <meteor.lxy@foxmail.com>
2020-09-01 11:34:41 +0800

    feat(eslint-config-vuepress-typescript): bump typescript-eslint version and update rules

--- packages/eslint-config-vuepress-typescript/index.js
+++ packages/eslint-config-vuepress-typescript/index.js
@@ -1,3 +1,5 @@
+const standard = require('eslint-config-standard')
+
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
@@ -8,13 +10,58 @@ module.exports = {
 
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    project: ['tsconfig.json', 'packages/**/tsconfig.json'],
+    extraFileExtensions: ['.vue'],
   },
 
   plugins: ['@typescript-eslint'],
 
   rules: {
+    'camelcase': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor':
+      standard.rules['no-array-constructor'],
+
+    'no-dupe-class-members': 'off',
+    '@typescript-eslint/no-dupe-class-members':
+      standard.rules['no-dupe-class-members'],
+
+    'no-implied-eval': 'off',
+    '@typescript-eslint/no-implied-eval': standard.rules['no-implied-eval'],
+
+    'no-throw-literal': 'off',
+    '@typescript-eslint/no-throw-literal': standard.rules['no-throw-literal'],
+
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions':
+      standard.rules['no-unused-expressions'],
+
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': standard.rules['no-unused-vars'],
+
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define':
+      standard.rules['no-use-before-define'],
+
     'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/no-useless-constructor':
+      standard.rules['no-useless-constructor'],
 
     '@typescript-eslint/explicit-function-return-type': [
       'warn',
