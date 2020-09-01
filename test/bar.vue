e1a27c5cf1941b11c10e5b3c58f6b0981e9a4d29
meteorlxy <meteor.lxy@foxmail.com>
2020-09-01 11:38:39 +0800

    test: add test dir

---
+++ test/bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <input v-model="bar" id="foo" />
+</template>
+
+<script lang="ts">
+const foobar: any = ""
+
+export default {
+  name: 'foo',
+
+  data: () => ({
+    bar: ''
+  }),
+}
+</script>
---
+++ test/foo.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+let foo_bar = "foo_bar"
+
+const FooBar = "FooBar"
+
+const FOO_BAR = "FOO_BAR"
+
+const fooBar = "fooBar"
+
+export default Vue.extend({
+  name: 'bar',
+})
