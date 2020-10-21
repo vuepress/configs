ae9055dddce1e5e645d66f8136c73e228afc6587
meteorlxy <meteor.lxy@foxmail.com>
2020-10-21 17:05:55 +0800

    feat(eslint-config-vuepress-typescript): update rules replacement

--- packages/eslint-config-vuepress-typescript/index.js
+++ packages/eslint-config-vuepress-typescript/index.js
@@ -1,5 +1,53 @@
 const standard = require('eslint-config-standard')
 
+/**
+ * Rules that have same name and config
+ * in bot eslint and @typescript/eslint
+ */
+const equivalents = [
+  'brace-style',
+  'comma-spacing',
+  'dot-notation',
+  'func-call-spacing',
+  'indent',
+  'keyword-spacing',
+  'lines-between-class-members',
+  'no-array-constructor',
+  'no-dupe-class-members',
+  // 'no-empty-function',
+  'no-extra-parens',
+  // 'no-extra-semi',
+  'no-implied-eval',
+  // 'no-loop-func',
+  // 'no-magic-numbers',
+  'no-redeclare',
+  // 'no-shadow',
+  'no-throw-literal',
+  'no-unused-expressions',
+  'no-unused-vars',
+  'no-use-before-define',
+  'no-useless-constructor',
+  'quotes',
+  'semi',
+  'space-before-function-paren',
+]
+
+/**
+ * Rules that should be turned off
+ *
+ * May need to be replaced by other rules
+ */
+const shouldOff = ['camelcase', 'no-new-func']
+
+const fromEntries = (arr) =>
+  arr.reduce(
+    (obj, [key, val]) => ({
+      ...obj,
+      [key]: val,
+    }),
+    {}
+  )
+
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
@@ -17,6 +65,16 @@ module.exports = {
   plugins: ['@typescript-eslint'],
 
   rules: {
+    ...fromEntries(shouldOff.map((name) => [name, 'off'])),
+
+    ...fromEntries(equivalents.map((name) => [name, 'off'])),
+    ...fromEntries(
+      equivalents.map((name) => [
+        `@typescript-eslint/${name}`,
+        standard.rules[name],
+      ])
+    ),
+
     'camelcase': 'off',
     '@typescript-eslint/naming-convention': [
       'error',
@@ -34,34 +92,28 @@ module.exports = {
       },
     ],
 
-    'no-array-constructor': 'off',
-    '@typescript-eslint/no-array-constructor':
-      standard.rules['no-array-constructor'],
-
-    'no-dupe-class-members': 'off',
-    '@typescript-eslint/no-dupe-class-members':
-      standard.rules['no-dupe-class-members'],
-
-    'no-implied-eval': 'off',
-    '@typescript-eslint/no-implied-eval': standard.rules['no-implied-eval'],
-
-    'no-throw-literal': 'off',
-    '@typescript-eslint/no-throw-literal': standard.rules['no-throw-literal'],
-
-    'no-unused-expressions': 'off',
-    '@typescript-eslint/no-unused-expressions':
-      standard.rules['no-unused-expressions'],
-
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': standard.rules['no-unused-vars'],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': [
+      standard.rules['comma-dangle'][0],
+      {
+        ...standard.rules['comma-dangle'][1],
+        enums: standard.rules['comma-dangle'][1].arrays,
+        generics: standard.rules['comma-dangle'][1].arrays,
+        tuples: standard.rules['comma-dangle'][1].arrays,
+      },
+    ],
 
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define':
-      standard.rules['no-use-before-define'],
-
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor':
-      standard.rules['no-useless-constructor'],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: false,
+        variables: false,
+        enums: false,
+        typedefs: false,
+      },
+    ],
 
     '@typescript-eslint/explicit-function-return-type': [
       'warn',
