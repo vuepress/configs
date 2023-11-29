059abc14847623e3830eea4f3becbed126522220
meteorlxy <meteor.lxy@foxmail.com>
2023-11-29 17:43:22 +0800

    feat(eslint-config-vuepress): add globals definition

--- packages/eslint-config-vuepress/lib/index.js
+++ packages/eslint-config-vuepress/lib/index.js
@@ -1,6 +1,16 @@
 module.exports = {
   extends: ['standard', 'plugin:import/recommended', 'prettier'],
 
+  globals: {
+    __VUEPRESS_VERSION__: 'readonly',
+    __VUEPRESS_BASE__: 'readonly',
+    __VUEPRESS_DEV__: 'readonly',
+    __VUEPRESS_SSR__: 'readonly',
+    __VUE_HMR_RUNTIME__: 'readonly',
+    __VUE_OPTIONS_API__: 'readonly',
+    __VUE_PROD_DEVTOOLS__: 'readonly',
+  },
+
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
