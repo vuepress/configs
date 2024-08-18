502f951e1c09dd3dd2d1696130242115e796428c
meteorlxy <meteor.lxy@foxmail.com>
2024-08-18 21:35:49 +0800

    build: bump deps and revert some style changes

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,13 +2,13 @@ import * as fs from 'node:fs'
 import * as path from 'node:path'
 import type { UserConfig } from '@commitlint/types'
 
-const PACKAGES = fs.readdirSync(path.resolve(__dirname, 'packages'))
-const SCOPES = PACKAGES.map((name) => name.replace(/-vuepress$/, ''))
+const packages = fs.readdirSync(path.resolve(__dirname, 'packages'))
+const scopes = packages.map((name) => name.replace(/-vuepress$/, ''))
 
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', SCOPES],
+    'scope-enum': [2, 'always', scopes],
     'footer-max-line-length': [0],
   },
 } satisfies UserConfig
