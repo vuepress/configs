18ee4b9d105d55d1749bd88ea405ff8988ad20d1
meteorlxy <meteor.lxy@foxmail.com>
2024-08-18 22:28:20 +0800

    fix(eslint-config): remove __VUE_OPTIONS_API__ from globals

--- packages/eslint-config-vuepress/src/vuepress.ts
+++ packages/eslint-config-vuepress/src/vuepress.ts
@@ -51,7 +51,6 @@ export const vuepress = async (
           __VUEPRESS_DEV__: 'readonly',
           __VUEPRESS_SSR__: 'readonly',
           __VUE_HMR_RUNTIME__: 'readonly',
-          __VUE_OPTIONS_API__: 'readonly',
           __VUE_PROD_DEVTOOLS__: 'readonly',
         },
       },
