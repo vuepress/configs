72d2cddb41c2194fded08cc66c3d094fb989edf0
meteorlxy <meteor.lxy@foxmail.com>
2025-08-11 16:17:04 +0800

    build: migrate to tsdown

---
+++ packages/prettier-config-vuepress/tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  dts: true,
+  entry: './src/index.ts',
+  exports: true,
+  fixedExtension: true,
+  format: ['cjs', 'esm'],
+  outDir: 'dist',
+  platform: 'node',
+})
